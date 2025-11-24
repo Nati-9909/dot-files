d35b2c5fcbb8ab31a6dc587810d7139023123cd7
Nati-9909 <natiayu4@gmail.com>
2025-11-24 14:09:41 +0300

    Added alias configs

---
+++ alias.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+alias nv=nvim
+alias fzf="fzf --height 40%"
+alias nload="nload devices wlo1"
+alias py="python3"
+alias rust-gdb="rust-gdb -q"
+
+alias bookmap='~/Temporary/bookmap_install-7.6.0_b28/bookmap_runscript.sh'
+
+alias ls="eza --icons=always  --no-filesize --no-user --no-time"
+alias ll="eza --icons=always --long --all  --no-filesize --no-user --no-time"
+alias ld="eza --icons=always --long --all --only-dirs  --no-filesize --no-user --no-time"
+alias lf="eza --icons=always --long --all --only-files  --no-filesize --no-user --no-time"
+
+alias zlib="sh /opt/Z-Library/z-library"
